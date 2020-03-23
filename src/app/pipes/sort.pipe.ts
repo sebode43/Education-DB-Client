5614a658d10b2ea108b878af88b929e595961e41
max-student <max-student@maxtrain.com>
2020-03-23 16:36:09 -0400

    Sort added

--- src/app/pipes/sort.pipe.ts
+++ src/app/pipes/sort.pipe.ts
@@ -5,8 +5,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class SortPipe implements PipeTransform {
 
-  transform(arr any[], sortBy: string): unknown {
-    return null;
+  transform(items: any[], sortBy: string): unknown {
+    if(items == null || items.length == 0) return items;
+    let sortFn = (a: any, b: any): number => {
+      let x = this.setValue(a[sortBy]); //the value of one column in that instance
+      let y = this.setValue(b[sortBy]);
+      if(x===y) return 0;
+      return(x<y)?-1:1;
+    }
+    return items.sort(sortFn);
   }
-
+  setValue = (colValue: any): any => {
+    if(colValue == null) return "";
+    if(typeof colValue == "number") return colValue;
+    return colValue.toString().toLowerCase();
+  };
 }
